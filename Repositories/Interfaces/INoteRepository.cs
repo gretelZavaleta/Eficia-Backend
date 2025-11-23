60a2583a0edd2512667dd3b853b3068c2c7613f1
Jesús Ojeda <ojedajesus232@gmail.com>
2025-11-23 18:38:30 -0500

    feat: add DTOs and Repositories for the data layer

---
+++ Repositories/Interfaces/INoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using EficiaBackend.Models;
+
+namespace EficiaBackend.Repositories.Interfaces
+{
+    public interface INoteRepository
+    {
+       
+        public Task<IEnumerable<Note>> GetNotesAsync();
+        public Task<Note?> GetNoteByIdAsync(int id);
+        public Task<Note> CreateNoteAsync(Note note);
+        public Task<Note> UpdateNoteAsync(Note note);
+        public Task<bool> DeleteNoteAsync(int id);
+
+    }
+}
