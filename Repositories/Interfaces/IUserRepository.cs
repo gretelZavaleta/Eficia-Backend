1e4fff8b0db417b12f283072fa89869255a3d779
gretelZavaleta <212823649+gretelZavaleta@users.noreply.github.com>
2025-11-23 18:30:22 -0500

    feat: new UserRepository and IUserRepository

---
+++ Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EficiaBackend.Models;
+
+namespace EficiaBackend.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task<User?> GetByIdAsync(int id);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User> CreateAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<bool> DeleteAsync(int id);
+    }
+}
