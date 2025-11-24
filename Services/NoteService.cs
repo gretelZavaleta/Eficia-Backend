b83c5663b2e90dd2d3f07bd04abe891f30076d81
Jesús Ojeda <ojedajesus232@gmail.com>
2025-11-23 19:06:55 -0500

    feat: add Methods in service (Gets & create)

--- Services/NoteService.cs
+++ Services/NoteService.cs
@@ -1,6 +1,40 @@
-﻿namespace EficiaBackend.Services
+﻿using EficiaBackend.Dtos.Notes;
+using EficiaBackend.Repositories.Interfaces;
+using EficiaBackend.Services.Interfaces;
+using System.Linq;
+namespace EficiaBackend.Services
 {
-    public class NoteService
+    public class NoteService : INoteService
     {
+        private readonly INoteRepository _Repository;
+        public NoteService(INoteRepository InoteRepository)
+        {
+            this._Repository = InoteRepository;
+        }
+        public async Task<IEnumerable<NoteDto>> GetAll()
+        {
+            await _Repository.GetNotesAsync();
+                   
+        }
+        public Task<NoteDto?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<NoteDto> Create(CreateNoteDto createNoteDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NoteDto?> Update(int id,UpdateNoteDto updateNoteDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
